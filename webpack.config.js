f3c7d2ee900197e115b8b0585ec67589f77c4a09
Uladzimir Halushka <Uladzimir_Halushka@epam.com>
2016-09-10 17:02:52 +0300

    Feature/webpack (#3)
    * add webpack build config
    
    * add last line

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: "./client/src/index.js",
+    output: {
+        path: __dirname + '/client/dist',
+        filename: "scripts/scripts.bundle.js"
+    },
+    devtool: isDevelopment ? 'source-map' : null,
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.styl$/,
+                loader: 'style-loader!css-loader!stylus-loader'
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug'
+            }
+        ]
+    },
+    watch: isDevelopment,
+    //generate html template for application
+    plugins: [new HtmlWebpackPlugin({
+        title: 'WSC',
+        template: './client/src/index.template.pug'
+    })]
+};
