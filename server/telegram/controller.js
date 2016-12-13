d66fe9155d9b4d13d6de1b249527bc5f48436c52
Maksim Shastsel <max.shastik@yandex.ru>
2016-12-13 11:00:14 +0300

    refactoring of bot (#46)
    * Refactoring of bot
    
    * Add redux-action dependency

--- server/telegram/controller.js
+++ server/telegram/controller.js
@@ -9,41 +9,35 @@ function isAvailToControllBot(username) {
     return users.indexOf(username) !== -1;
 }
 
-export default {
+const controller = {
     stat(bot, msg) {
-        cli.log(`/stat recived from ${msg.from.username}`);
-        if(isAvailToControllBot(msg.from.username)) {
-            const state = store.getState();
-            const message = `${SystemSelectors.isGameStarted(state) ? 'Game started': 'Game did\'t started'}
-            Connected ${players(state).size} players`;
-            bot.sendMessage(msg.from.id, message);
-        } else {
-            bot.sendMessage(msg.from.id, 'perm denied');
-            bot.leaveChat(msg.chat.id);
-        }
+        const state = store.getState();
+        const message = `${SystemSelectors.isGameStarted(state) ? 'Game started': 'Game did\'t started'}Connected ${players(state).size} players`;
+        bot.sendMessage(msg.from.id, message);
     },
     start(bot, msg) {
-        cli.log(`/start recived from ${msg.from.username}`);
-        if(isAvailToControllBot(msg.from.username)) {
-            if(startGame()) {
-                cli.log(`Game started. Source of command telegram bot. Execute by @${msg.from.username}`);
-                return bot.sendMessage(msg.from.id, `Thx @${msg.from.username}, you successfully start the game`);
-            }
-            bot.sendMessage(msg.from.id, `Sorry @${msg.from.username}, game started before your command`);
-        } else {
-            bot.sendMessage(msg.from.id, 'perm denied');
-            bot.leaveChat(msg.chat.id);
+        if(startGame()) {
+            cli.log(`Game started. Source of command telegram bot. Execute by @${msg.from.username}`);
+            return bot.sendMessage(msg.from.id, `Thx @${msg.from.username}, you successfully start the game`);
         }
+        bot.sendMessage(msg.from.id, `Sorry @${msg.from.username}, game started before your command`);
     },
     finish(bot, msg) {
-        cli.log(`/finish recived from ${msg.from.username}`);
+        cli.log(`Game finished. Source of command telegram bot. Execute by @${msg.from.username}`);
+        finishGame();
+        return bot.sendMessage(msg.from.id, `Thx @${msg.from.username}, you successfully finish the game`);
+    }
+};
+
+export default Object.keys(controller).reduce((R, key) => {
+    R[key] = function(bot, msg) {
+        cli.log(`${msg.text} recived from ${msg.from.username}`);
         if(isAvailToControllBot(msg.from.username)) {
-            cli.log(`Game finished. Source of command telegram bot. Execute by @${msg.from.username}`);
-            finishGame();
-            return bot.sendMessage(msg.from.id, `Thx @${msg.from.username}, you successfully finish the game`);
+            return controller[key](bot, msg);
         } else {
             bot.sendMessage(msg.from.id, 'perm denied');
             bot.leaveChat(msg.chat.id);
         }
-    }
-};
+    };
+    return R;
+}, {});
