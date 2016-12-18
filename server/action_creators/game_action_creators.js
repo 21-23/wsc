6375efeff5c5f622cbb25ea44fc53db6d0eefce5
Maksim Shastsel <max.shastik@yandex.ru>
2016-12-18 18:29:05 +0300

    Apply end time to each solved task (#48)

--- server/action_creators/game_action_creators.js
+++ server/action_creators/game_action_creators.js
@@ -22,6 +22,7 @@ export function playerSolveTaskActionCreator(player) {
             [player.id]: {
                 currentTask: {},
                 taskSolved: player.taskSolved + 1,
+                end: Date.now(),
             }
         }
     };
