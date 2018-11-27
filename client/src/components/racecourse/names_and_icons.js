a3a4d46be4326064367b7a1643682b73ed5a7eaf
Maksim Shastsel <max@shas.tel>
2018-11-27 15:44:22 +0300

    Add 'function evaluation' task (#98)

--- client/src/components/racecourse/names_and_icons.js
+++ client/src/components/racecourse/names_and_icons.js
@@ -1,10 +1,11 @@
 export const names = [];
 
 names[0] = 'START';
-names[3] = 'FINISH';
+names[4] = 'FINISH';
 
 export const icons = [
     'directions_walk',
+    'directions_run',
     'directions_run',
     'directions_run',
     'sentiment_very_satisfied'
